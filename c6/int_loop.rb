75877a58cfeb665e71a7d2e31bbfadcdd01fb14e
wubuqila <wubuqilai@gmail.com>
2015-07-20 09:15:49 +0800

    add

--- c6/int_loop.rb
+++ c6/int_loop.rb
@@ -1,2 +1,12 @@
 p 5.times.methods
+p 5.times.methods[0].class
 p 5.instance_variables
+
+5.times{|item| print item , " "}
+puts
+5.upto(10) { |n| print n , " " }
+puts
+5.downto(-3){|n| print n, " "}
+puts
+1.step(100,5) { |n| print n, " " }
+puts
