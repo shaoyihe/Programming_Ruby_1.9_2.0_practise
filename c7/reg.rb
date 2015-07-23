fdb3a802a73bb76e28f704dfef37769e14bda287
wubuqila <wubuqilai@gmail.com>
2015-07-23 20:46:28 +0800

    m

--- c7/reg.rb
+++ c7/reg.rb
@@ -4,8 +4,8 @@ p /test/.all_super_class
 
 p /test/=~"atest b"
 
-File.open("../c8/block.rb").each do |line|
-	if /def/ =~ line
+File.open("./reg.rb").each do |line|
+	if /p/ =~ line
 		puts line
 	end
 end
