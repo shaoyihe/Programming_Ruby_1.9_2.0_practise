370f270856243e9a55b0bab166281974dccaec7f
shaoyi he <wubuqilai@gmail.com>
2015-07-23 23:29:18 +0800

    add

--- c7/reg.rb
+++ c7/reg.rb
@@ -4,8 +4,18 @@ p /test/.all_super_class
 
 p /test/=~"atest b"
 
-File.open("./reg.rb").each do |line|
+File.foreach("./reg.rb").with_index do |line, index|
 	if /p/ =~ line
-		puts line
+		puts "#{index} : #{line}"
 	end
 end
+
+puts "*"* 100
+
+target = "some test text"
+p target.sub(/[a-z]+/,"a")
+p target.gsub(/\s/,"a")
+p target.gsub(/[a-z]+/){|find| find + String(1)}
+
+puts "*"*100
+
---
+++ c9/num.rb
@@ -0,0 +1,10 @@
+class Fixnum
+	alias old_plus +
+	def + other
+		old_plus(Integer(other))
+	end
+end	
+
+p 1.class
+p 1.+"20000000000000"
+p 1.upto(10).to_a
